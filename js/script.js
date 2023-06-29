06bf7f007f1ca86878671cd2e2a32664a4d0819e
iv77777 <70826113+iv77777@users.noreply.github.com>
2023-06-29 12:33:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -637,7 +637,7 @@ function showWords() {
           setTimeout(() => {
             // Встановлює фокус на інпут
             setsAocusOnInputs();
-          }, 1000);
+          }, 200);
         }
       });
     }
